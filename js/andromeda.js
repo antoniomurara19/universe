6856483ee5733b7afb47177e6c1a96ae1cc709e0
Antônio Murara Zabala <antoniomurar4@gmail.com>
2023-05-17 00:01:40 -0300

    começando a responsividade

--- js/andromeda.js
+++ js/andromeda.js
@@ -3,6 +3,7 @@ const remove = document.getElementById('remove')
 const add_carrinho = document.getElementById('add-carrinho')
 const carrinho = document.querySelector('.carrinho')
 const carrinho_andromeda = document.querySelector('.container-carrinho-andromeda') 
+const body = document.getElementById('body')
 let contador = document.getElementById('contador')
 const fechar_carrinho = document.querySelector('.fechar-carrinho')
 let i=0
---
+++ menu.js
@@ -0,0 +1,6 @@
+let btn_btn = document.querySelector('.btn_btn')
+let navbar = document.querySelector('.navbar')
+
+btn_btn.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbar_ativo')
+})
